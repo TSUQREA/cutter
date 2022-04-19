bcae3b613ddff001d453624d6d1561b85c85e58b
Ninomiya Kan <ninomiyakan.dev@gmail.com>
2022-04-19 21:21:50 +0900

    [update] 不要なenvを削除

--- next.config.js
+++ next.config.js
@@ -2,15 +2,11 @@ require('dotenv').config()
 module.exports = {
     env: {
         serviceName: 'cutter', //Service Name
-        serviceNamePlain: 'cutter', //servicename
         serviceDomain: 'cutter.go5.run', //Domain
         serviceDescription: 'Google Spreadsheet-based ShortURL Service.', //Description
         serviceIcon: 'https://twemoji.maxcdn.com/v/latest/72x72/1f517.png', //Icon
 
         // Google SpreadSheet
-        SHEET_ID: "",
-        WORKSHEET_ID: 0,
-        GOOGLE_SERVICE_ACCOUNT_EMAIL: "MAIL",
-        GOOGLE_PRIVATE_KEY: "KEY"
+        SHEET_ID: ""
     }
 };
